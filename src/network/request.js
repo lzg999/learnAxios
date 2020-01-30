9dbca3086a9e1bc2b51ddfe4b2fca8863323666a
lzg999 <lzg940990149@outlook.com>
2020-01-30 11:24:39 +0800

    接口地址的更新

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export function request(config){
     // 1、 创建Axios的实例
     const instance = axios.create({
-      baseURL: 'http://123.207.32.32:8000/api/w1',
+      baseURL: 'http://123.207.32.32:8000/api/hy',
       timeout: 5000
     })
     // 发送真正的网络请求(返回的直接就是个promise对象)
